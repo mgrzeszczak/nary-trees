41898e8484465d260853351c3a19e2cacee47b88
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2018-07-20 12:12:25 +0200

    Generic case entry parser test

--- src/test/scala/com/github/mgrzeszczak/scalatask/EntryParserTest.scala
+++ src/test/scala/com/github/mgrzeszczak/scalatask/EntryParserTest.scala
@@ -11,12 +11,15 @@ class EntryParserTest extends FlatSpec with Matchers {
 
   it should "parse valid entries" in {
     forAll(Table(
-      ("a", "b", "c", "id", "expected"),
-      ("a", "", "", "1", Entry(1, "a", 0)),
-      ("", "b", "", "2", Entry(2, "b", 1)),
-      ("", "", "c", "3", Entry(3, "c", 2))
-    )) { (a, b, c, id, expected) =>
-      parser.fromRow(Array(a,b,c,id)) shouldEqual expected
+      ("a", "b", "c", "d", "e", "id", "expected"),
+      ("a", "", "", "", "", "1", Entry(1, "a", 0)),
+      ("", "b", "", "", "", "2", Entry(2, "b", 1)),
+      ("", "", "c", "", "", "3", Entry(3, "c", 2)),
+      ("", "", "", "d", "", "4", Entry(4, "d", 3)),
+      ("", "", "", "", "e", "5", Entry(5, "e", 4))
+
+    )) { (a, b, c, d, e, id, expected) =>
+      parser.fromRow(Array(a, b, c, d, e, id)) shouldEqual expected
     }
   }
 
